252b2815138f3c060c8b303dcbd4f31f268051a9
anobi <nsalakka@gmail.com>
2016-01-23 07:59:23 +0200

    shooting is fun

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,25 +5,44 @@ public class Player : MonoBehaviour {
 
     public float moveSpeed;
 
-    private Vector2 movement, aimDirection, position, mousePosition;
-    private bool aiming;
+    private Vector2 movement, position, mousePosition, aimDirection;
+    private bool aiming, weaponReady;
+
+    public GameObject bullet;
 
 	// Use this for initialization
 	void Start () {
+        bullet = Resources.Load("Bullet") as GameObject;
         position = new Vector2(transform.position.x, transform.position.y);
         mousePosition = new Vector2();
+        weaponReady = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
         position.Set(transform.position.x, transform.position.y);
-        mousePosition.Set(Input.mousePosition.x, Input.mousePosition.y);
+
+        var mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        mousePosition.Set(mouse.x, mouse.y);
         aimDirection = (mousePosition - position).normalized;
 
         HandleMovement();
-        if (Input.GetButton("Fire"))
+
+        if (Input.GetButton("Aim"))
             StartCoroutine(Aim());
+
+        if (Input.GetButton("Fire") && weaponReady)
+        {
+            Shoot();
+            if (aiming) weaponReady = false;
+        }
+
+        if (Input.GetButtonUp("Fire") && !weaponReady)
+        {
+            weaponReady = true;
+        }
 	}
 
     void FixedUpdate(){
@@ -40,14 +59,32 @@ public class Player : MonoBehaviour {
 
     private void Shoot()
     {
+            weaponReady = !aiming;
+
+            GameObject projectile = Instantiate(bullet) as GameObject;
+            projectile.transform.position = position;
+            var rb = projectile.GetComponent<Rigidbody2D>();
+            rb.simulated = true;
+
+            rb.AddForce(aimDirection * 2000);
+
+            if (!aiming)
+            {
+                var scatter = Mathf.Sin(Time.frameCount / 2) * GetRandom();
+                rb.AddForce(new Vector2(scatter, scatter) * 500);
+            }
+    }
+
+    private static float GetRandom(){
+        return Random.Range(0.1f, 1.0f);
     }
 
     IEnumerator Aim()
     {
-        while (Input.GetButton("Fire"))
+        while (Input.GetButton("Aim"))
         {
             aiming = true;
-            var raycast = Physics2D.Raycast(position, Camera.main.ScreenToWorldPoint(mousePosition));
+            var raycast = Physics2D.Raycast(position, aimDirection);
             if (raycast.collider != null)
             {
                 var hit = raycast.point;
@@ -59,4 +96,6 @@ public class Player : MonoBehaviour {
         }
         aiming = false;
     }
+
+
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,21 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Projectile : MonoBehaviour {
-
-
+public class Projectile : MonoBehaviour 
+{
+    BoxCollider2D coll;
+    float spawnTime;
 	// Use this for initialization
 	void Start () {
-	
+        spawnTime = Time.fixedTime;
+        coll = GetComponent<BoxCollider2D>();
+        coll.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if((Time.fixedTime - spawnTime) > 0.025f){
+            coll.enabled = true;
+        }
 	}
 
-    void OnCollisionEnter()
+    void OnCollisionEnter2D(Collision2D c)
     {
+        Debug.Log("collision");
+        var target = c.gameObject;
+        var item = target.GetComponent<Item>();
 
+        if (item != null)
+        {
+            item.OnShot();
+        }
+
+        Destroy(this.gameObject);
     }
 }
