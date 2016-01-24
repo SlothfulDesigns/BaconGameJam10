9688ba0e1841a9fdef85c45a598e98522640c0c4
anobi <nsalakka@gmail.com>
2016-01-24 19:49:15 +0200

    quest list

---
+++ Assets/Scripts/QuestMenu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class QuestMenu : MonoBehaviour {
+
+    Level level;
+    QuestMenu menu;
+
+	// Use this for initialization
+	void Start () {
+        level = FindObjectOfType<Level>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var items = GetComponent<Text>();
+        var quests = level.quests;
+        var list = "";
+
+        foreach (var q in quests)
+        {
+            var completed = " ";
+
+            if (q.IsCompleted())
+                completed = "X";
+
+            list += "\n[" + completed + "] " + q.name;
+        }
+        items.text = list;
+	}
+}
