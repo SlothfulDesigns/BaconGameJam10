15f488ae22829aa04086c1a77f0cb7ae87306daf
Antti Salo <antti.asa.salo@gmail.com>
2016-01-24 21:53:03 +0200

    add WIP credits scene

---
+++ Assets/Scripts/EndCredits.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndCredits : MonoBehaviour {
+
+	public GameObject Camera;
+	public int speed = 1;
+	public string level;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		Camera.transform.Translate(Vector2.down * Time.deltaTime * speed);
+	}
+
+	IEnumerator waitFor()
+	{
+		yield return new WaitForSeconds (20);
+		Application.LoadLevel (level);
+	}
+}
